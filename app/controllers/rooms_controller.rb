7801ce3b891323cecb4d6ef6ba83bc6beced45b3
Ayub Ansary <ayubuap35@gmail.com>
2023-06-19 10:10:59 +0600

    Creating chat room for users

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @rooms = Room.public_rooms
+    @users = User.all_except(current_user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :rooms
   root 'pages#home'
   devise_scope :user do
     # Redirects signing out users back to sign-in
     get 'users', to: 'devise/sessions#new'
   end
-
+  # Redirects to user account for private chat
+  get 'user/:id', to: 'users#show', as: 'user'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
